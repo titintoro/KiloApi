a18a27f0e0bf8586939512ce271fc08e425491f5
davidgm26 <davidgama260402@gmail.com>
2022-12-14 18:49:15 +0100

    Entidades creadas

---
+++ KiloApi/src/main/java/com/salesianostriana/dam/kiloapi/kilosDisp/kilosDisp.java
@@ -0,0 +1,20 @@
+package com.salesianostriana.dam.kiloapi.kilosDisp;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Getter  @Setter
+public class kilosDisp {
+
+    @ManyToOne(cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.EAGER)
+    @JoinColumn(name = "tipoAlimento_id" , foreignKey = @ForeignKey(name = "FK_KILOSDISP_TIPOALIMENT"))
+    @MapsId(tipoAlimento_id)
+    private TipoAlimento id;
+
+    private double cantidadDisponible;
+
+}
