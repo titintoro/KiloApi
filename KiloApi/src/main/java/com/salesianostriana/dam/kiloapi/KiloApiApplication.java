9d6e15f8b385efee1601341e359dc599db912f93
titintoro <titintola@gmail.com>
2022-12-14 10:46:34 +0100

    Creando Proyecto

---
+++ KiloApi/src/main/java/com/salesianostriana/dam/kiloapi/KiloApiApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.kiloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiloApiApplication.class, args);
+	}
+
+}
