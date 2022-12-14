fb85f0cebf2951d5527f6df11c10d9dd9224330e
JorgeTenorio96 <jorgetenorio96@gmail.com>
2022-12-14 18:53:22 +0100

    Creadas Entidades Aportacion y DetalleAportacion

---
+++ KiloApi/src/main/java/com/salesianostriana/dam/kiloapi/detalleAportacion/DetalleAportacion.java
@@ -0,0 +1,30 @@
+package com.salesianostriana.dam.kiloapi.detalleAportacion;
+
+import com.salesianostriana.dam.kiloapi.aportacion.Aportacion;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class DetalleAportacion {
+
+    @Id
+    @GeneratedValue
+    private Long numLinea;
+
+    @ManyToOne
+    @JoinColumn(name = "Aportacion", foreignKey = @ForeignKey(name = "FK_DETALLEAPORTACION_APORTACION"))
+    private Aportacion idAportacion;
+
+    private double cantidad;
+
+    @ManyToOne
+    @JoinColumn(name = "tipoAlimento_id", foreignKey = @ForeignKey(name = "FK_TIPOALIMENTO_DETALLEAPORTACION"))
+    private TipoAlimento idTipoAlimento;
+
+}
