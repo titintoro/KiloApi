5c329171daddf3660bd8316f9f591e810c361e33
JorgeTenorio96 <jorgetenorio96@gmail.com>
2022-12-14 20:34:32 +0100

    Añadidos Repositorios y Servicios de Aportacion y DetallesAportacion

---
+++ KiloApi/src/main/java/com/salesianostriana/dam/kiloapi/aportacion/AportacionServicio.java
@@ -0,0 +1,36 @@
+package com.salesianostriana.dam.kiloapi.aportacion;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AportacionServicio {
+
+    private final AportacionRepositorio repositorio;
+
+    public Aportacion add(Aportacion aportacion){return repositorio.save(aportacion);}
+
+    public Optional<Aportacion> findById(Long id) {
+        return repositorio.findById(id);
+    }
+
+    public List<Aportacion> findAll() {
+        return repositorio.findAll();
+    }
+
+    public Aportacion edit(Aportacion aportacion) {
+        return repositorio.save(aportacion);
+    }
+
+    public void delete(Aportacion aportacion) {
+        repositorio.delete(aportacion);
+    }
+
+    public void deleteById(Long id) {
+        repositorio.deleteById(id);
+    }
+}
