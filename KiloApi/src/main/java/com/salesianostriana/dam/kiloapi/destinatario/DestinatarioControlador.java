6e2bdc1d4e5438b9126c830573321e9a05d0e576
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-12-16 10:45:41 +0100

    Documentando la clase DestinatarioControlador

--- KiloApi/src/main/java/com/salesianostriana/dam/kiloapi/destinatario/DestinatarioControlador.java
+++ KiloApi/src/main/java/com/salesianostriana/dam/kiloapi/destinatario/DestinatarioControlador.java
@@ -2,6 +2,13 @@ package com.salesianostriana.dam.kiloapi.destinatario;
 
 import com.salesianostriana.dam.kiloapi.caja.Caja;
 import com.salesianostriana.dam.kiloapi.caja.CajaServicio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,21 +26,93 @@ public class DestinatarioControlador {
     private DestinatarioServicio servicio;
     private CajaServicio servicioCaja;
 
+    @Operation(summary = "Este método devuelve una lista de destinatarios")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha encontrado la lista de destinatarios",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Destinatario.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            [
+                                            
+                                            ]                                          
+                                            """
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha encontrado ninguna lista de destinatarios",
+                    content = @Content),
+    })
     @GetMapping("/destinatario/")
     public ResponseEntity<List<Destinatario>> findAllDestinatarios(){
         return ResponseEntity.ok(servicio.findAll());
     }
 
+    @Operation(summary = "Este método devuelve una lista de destinatarios por su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se han encontrado la lista de destinatarios por su id",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Destinatario.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            [
+                                            
+                                            ]                                          
+                                            """
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha encontrado ninguna lista de destinatarios por su id",
+                    content = @Content),
+    })
     @GetMapping("/destinatario/{id}")
     public ResponseEntity<Destinatario> findByIdDestinatarios(@PathVariable Long id){
         return ResponseEntity.of(servicio.findById(id));
     }
 
+    @Operation(summary = "Este método devuelve los detalles una lista de destinatarios por su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se han encontrado la lista de reproducción por su id",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Destinatario.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            [
+                                            
+                                            ]                                          
+                                            """
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se han encontrado los detalles de ninguna lista de destinatarios por su id",
+                    content = @Content),
+    })
     @GetMapping("destinatario/{id}/detalle")
     public ResponseEntity<Destinatario> findByIdDestinatarioDetalle(@PathVariable Long id){
         return ResponseEntity.of(servicio.findById(id));
     }
 
+    @Operation(summary = "Este método devuelve una lista de reproducción por su id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se han encontrado la lista de reproducción por su id",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Destinatario.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            [
+                                            
+                                            ]                                          
+                                            """
+                            )}
+                    )}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se han encontrado los detalles de  ninguna lista de reproducción por su id",
+                    content = @Content),
+    })
     @PostMapping("/destinatario/")
     public ResponseEntity<Destinatario> createDestinatario(@RequestBody Destinatario destinatario){
         return ResponseEntity.status(HttpStatus.CREATED).body(servicio.add(destinatario));
